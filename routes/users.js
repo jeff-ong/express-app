b22b76f063b14d2c847bab0f9752102972fcbf3b
jeff-ong <jeff-ong@outlook.com>
2019-01-13 11:59:08 -0500

    tighten code spacing

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
 const spotify_util = require('../api/spotify');
 
 router.get('/api/spotify',function(req,res,next){
   const task = req.query.task;
   const query = req.query.query;
-
   spotify_util(task,query).then((data)=>{
     res.json(data);
   });
-  
 });
 
 router.get('/', function(req, res, next) {
